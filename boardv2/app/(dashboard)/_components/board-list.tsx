d6d9ce814eb4c4cc1886d1c2a40dfdac323f02e6
prem-web.dev <a70064182@gmail.com>
2026-05-11 20:58:11 +0530

    Update board-list.tsx

--- boardv2/app/(dashboard)/_components/board-list.tsx
+++ boardv2/app/(dashboard)/_components/board-list.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-
-
+import { Loader } from "lucide-react";
 import { useQuery } from "convex/react";
 import { EmptyBoards } from "./empty-boards";
 import { EmptyFavorites } from "./empty-favorites";
@@ -9,7 +8,6 @@ import { EmptySearch } from "./empty-search";
 import { api } from "@/convex/_generated/api";
 import { BoardCard } from "./board-card";
 import { NewBoardButton } from "./new-board-button";
-import { Doc } from "@/convex/_generated/dataModel";
 
 interface BoardListProps{
     orgId: string;
@@ -18,8 +16,6 @@ interface BoardListProps{
         favorites? : string;
     };
 }
-
-type BoardWithFavorite = Doc<"boards"> & { isFavorite: boolean };
 
 export const BoardList = ({
     orgId, 
@@ -33,16 +29,20 @@ export const BoardList = ({
 
 if (data === undefined) {
   return (
-    <div>
+    <div className="relative">
       <h2 className="text-3xl font-semibold">
         {query.favorites ? "Favorite boards" : "Team boards"}
       </h2>
+
+      <div className="absolute inset-0 flex items-center justify-center z-10 pointer-events-none">
+        <Loader className="w-8 h-8 text-muted-foreground animate-spin" />
+      </div>
 
       <div
         className="
           grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 
           lg:grid-cols-5 xl:grid-cols-6
-          gap-5 mt-8 pb-10
+          gap-5 mt-8 pb-10 opacity-50
         "
       >
         {!query.favorites && (
@@ -84,7 +84,7 @@ if (data === undefined) {
                 {!query.favorites && (
                  <NewBoardButton orgId={orgId} disabled={false}  />
                 )}
-                {data?.map((board: BoardWithFavorite)=>(
+                {data?.map((board)=>(
                     <BoardCard
                      key={board._id}
                      id={board._id}
